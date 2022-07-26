7c9d1378f9be7c2ec21420c784a3a7e6871acf76
B. A. Ballesteros <104979664+AlfabetaWeb@users.noreply.github.com>
2022-07-26 09:06:33 +0200

    feat: install jwt library

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/// ELIMINATE THIS THREE LINES, WE WILL NOT USE THEM
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/// ELIMINATE THIS THREE LINES, WE WILL NOT USE THEM
+// Route::get('/', function () {
+//     return view('welcome');
+// });
