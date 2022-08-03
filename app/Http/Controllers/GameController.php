46120e056c2205fd873e35a6cb22dc351a55ede8
B. A. Ballesteros <104979664+AlfabetaWeb@users.noreply.github.com>
2022-08-03 15:46:49 +0200

    Create links to related files

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Game;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\Http\Controllers\User; 
 
 class UserController extends Controller
 {
